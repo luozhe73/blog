cdaad2380ee2b010d70fd5d389fc00407539cdc2
luozhe73 <Lws1cqZ>
2020-04-27 19:58:22 +0800

    修复后台管理员添加主键报错

--- src/main/java/com/cvicse/blog/po/Blog.java
+++ src/main/java/com/cvicse/blog/po/Blog.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class Blog {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
